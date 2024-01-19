bb1cc57e8547c405220b3e934803ee0277f6f167
Ashalkito <aitorclementeramoswow@gmail.com>
2024-01-19 14:02:44 +0100

    Otros nuevos cambios

--- miprimermodulo/__manifest__.py
+++ miprimermodulo/__manifest__.py
@@ -14,7 +14,7 @@ Esto es un módulo creado para una práctica de SGE
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -30,5 +30,7 @@ Esto es un módulo creado para una práctica de SGE
     'demo': [
         'demo/demo.xml',
     ],
+    "application": True,
+    "installable": True
 }
 
